5cd4209035769361978a6b340f1099f1d5750691
hernian <hernianrunner@gmail.com>
2025-01-17 13:33:05 +0900

    プロジェクト ファイルを追加します。

---
+++ narozip2mobi/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace narozip2mobi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
